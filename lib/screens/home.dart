106c043314cfbc380edb9c5c8782ca1def2cbc84
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-03-06 09:21:02 +0545

    custom search bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 
 import 'package:flutter_commerce/widgets/circular_container.dart';
 import 'package:flutter_commerce/utils/constants/colors.dart';
+import 'package:flutter_commerce/widgets/custom_app_bar.dart';
 import 'package:flutter_commerce/widgets/custom_curved_clipper.dart';
+import 'package:flutter_commerce/utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,14 +21,54 @@ class HomeScreen extends StatelessWidget {
               child: Container(
                 padding: const EdgeInsets.all(0),
                 color: TColors.primary,
-                child: const SizedBox(
+                child: SizedBox(
                   height: 400,
                   child: Stack(
                     children: [
-                      Positioned(
+                      const Positioned(
                           top: -150, right: -250, child: CircularContainer()),
-                      Positioned(
+                      const Positioned(
                           top: 100, right: -300, child: CircularContainer()),
+                      CustomAppBar(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              TTexts.homeAppbarTitle,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelMedium!
+                                  .copyWith(color: TColors.grey),
+                            ),
+                            const SizedBox(height: 2),
+                            Text(
+                              TTexts.homeAppbarSubTitle,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall!
+                                  .copyWith(color: TColors.white),
+                            )
+                          ],
+                        ),
+                        actions: [
+                          IconButton(
+                              onPressed: () {},
+                              icon: Badge(
+                                label: Text(
+                                  '2',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .copyWith(color: TColors.white),
+                                ),
+                                backgroundColor: TColors.black,
+                                child: const Icon(
+                                  Iconsax.shopping_bag,
+                                  color: TColors.white,
+                                ),
+                              ))
+                        ],
+                      ),
                     ],
                   ),
                 ),
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_commerce/utils/constants/sizes.dart';
+import 'package:flutter_commerce/utils/device/device_utility.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    this.title,
+    this.showBackArrow = false,
+    this.leadingIcon,
+    this.actions,
+    this.onLeadingPressed,
+  });
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? onLeadingPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: TSizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: showBackArrow
+            ? IconButton(
+                onPressed: () {}, icon: const Icon(Iconsax.arrow_right))
+            : leadingIcon != null
+                ? IconButton(onPressed: () {}, icon: Icon(leadingIcon))
+                : null,
+        title: title,
+        actions: actions,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
+}
